98eec37ece777caf60b46b3cda68f5c17a4edfb4
arcanlte <pinangayjoel@gmail.com>
2020-04-09 21:20:06 +0800

    Made changes into my model.js

--- pokemon-backend/models.js
+++ pokemon-backend/models.js
@@ -1,12 +1,22 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  database: "pokemon_db",
-  dialect: "postgres",
-  define: {
-    underscored: true
-  }
-});
+let sequelize;
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    define: {
+      underscored: true,
+    },
+  });
+} else {
+  sequelize = new Sequelize({
+    database: "pokemon_db",
+    dialect: "postgres",
+    define: {
+      underscored: true,
+    },
+  });
+}
 
 class Pokemon extends Sequelize.Model {}
 class Move extends Sequelize.Model {}
@@ -16,11 +26,11 @@ User.init(
   {
     username: Sequelize.STRING,
     password_digest: Sequelize.STRING,
-    rank: Sequelize.STRING
+    rank: Sequelize.STRING,
   },
   {
     sequelize,
-    modelName: "user"
+    modelName: "user",
   }
 );
 
@@ -35,11 +45,11 @@ Pokemon.init(
     total_experience: Sequelize.INTEGER,
     current_experience: Sequelize.INTEGER,
     fullyEvolved: Sequelize.BOOLEAN,
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   {
     sequelize,
-    modelName: "pokemon"
+    modelName: "pokemon",
   }
 );
 
@@ -48,11 +58,11 @@ Move.init(
     name: Sequelize.TEXT,
     attack: Sequelize.INTEGER,
     animation: Sequelize.STRING,
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   {
     sequelize,
-    modelName: "move"
+    modelName: "move",
   }
 );
 
@@ -65,5 +75,5 @@ module.exports = {
   Pokemon,
   Move,
   User,
-  sequelize
+  sequelize,
 };
